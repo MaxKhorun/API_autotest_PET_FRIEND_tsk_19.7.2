52e927cf42e499aa2385e1a25b6022024316db0b
M.Kh <sea-of-max@yandex.ru>
2023-06-23 02:09:15 +0300

    Updated files

--- api_catalog.py
+++ api_catalog.py
@@ -23,6 +23,7 @@ class PetFriends:
             result = res.json()
         except:
             result = res.text
+        print(result)
         return status, result
         pass
     def get_list_of_pest(self, auth_key: json, filter: str) -> json:
@@ -35,13 +36,14 @@ class PetFriends:
         filter = {
             'filter': filter
         }
-        res = requests.get(self.base_url+'api/pets', headers=header, params=json.dumps(filter))
+        res = requests.get(self.base_url+'api/pets', headers=header, params=filter)
         status = res.status_code
         result = ""
         try:
             result = res.json()
         except:
             result = res.text
+        print(result)
         return status, result
     def post_newPet(self, auth_key: json, name: str, pet_type: str, age: str, pet_photo: str) -> json:
         ''''Добавляет питомца через отправку запроса к API;
@@ -67,7 +69,7 @@ class PetFriends:
             result = res.text
         print(result)
         return status, result
-    def delete_pet(self, auth_key: json, pet_ID: int) -> json:
+    def delete_pet(self, auth_key: json, pet_ID: str) -> json:
         '''Метод удаляет питомца. Принмает ID, также нужен уникальный API_key'''
         header = {
             'accept' : 'application/json',
@@ -96,6 +98,26 @@ class PetFriends:
         }
 
         res = requests.post(self.base_url + 'api/create_pet_simple', headers=header, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        print(result)
+        return status, result
+    def upload_photo(self, auth_key: json, pet_ID: int, pet_photo: str) -> json:
+
+        data = MultipartEncoder(
+            fields = {
+                'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+            })
+        header = {'accept': 'application/json',
+                   'auth_key': auth_key['key'],
+                   'Content-Type': data.content_type
+                  }
+        res = requests.post(self.base_url + f'api/pets/set_photo/{pet_ID}',
+                            headers=header, data=data)
         status = res.status_code
         result = ""
         try:
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,6 +1,6 @@
 import os
-from api_catalog import PetFriends
-from settings import login_email, login_pass
+from ..api_catalog import PetFriends
+from ..settings import login_email, login_pass
 
 pf = PetFriends()
 '''тест с получением ключа с сервера для работы с прочими АПИ-командами'''
@@ -25,14 +25,35 @@ def test_post_new_pet(name='Viktor', pet_type='Canary', age='4', pet_photo='imag
     assert status == 200
     assert result['name'] == name
 
+def test_new_pet_wtht_photo(name='Duran_12', pet_type='Cat', age='4'):
+
+    _, auth_key = pf.get_api_key(login_email, login_pass)
+
+    status, result = pf.create_simple_pet(auth_key, name, pet_type, age)
+    assert status == 200
+    assert result['name'] == name
+
+def test_added_photo(pet_photo='images\kenar-vitek.jpg'):
+
+    _, auth_key = pf.get_api_key(login_email, login_pass)
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, no_photo_pet = pf.create_simple_pet(auth_key, 'Pet Friend N1', 'friend', '2')
+    pet_ID = no_photo_pet['id']
+    status, result = pf.upload_photo(auth_key, pet_ID, pet_photo)
+
+    assert  status == 200
+    assert len(result['pet_photo']) > 0
+
+
 '''[DELETE] / api / pets / {pet_id} — удаление питомца из базы данных;'''
 def test_delete_pet():
     _, auth_key = pf.get_api_key(login_email, login_pass)
     _, my_pets = pf.get_list_of_pest(auth_key, 'my_pets')
 
-    if len(my_pets['pets']) == 0:
-        pf.post_newPet(auth_key, 'Some_pet_to_test', 'Canary', '3', 'images\kenar-vitek.jpg')
-        _, my_pets = pf.get_list_of_pest(auth_key, 'my_pets')
+    # if len(my_pets['pets']) == 0:
+    #     pf.post_newPet(auth_key, 'Some_pet_to_test', 'Canary', '3', 'images\kenar-vitek.jpg')
+    #     _, my_pets = pf.get_list_of_pest(auth_key, 'my_pets')
 
     pet_ID = my_pets['pets'][0]['id']
     status, _ = pf.delete_pet(auth_key, pet_ID)
@@ -42,11 +63,4 @@ def test_delete_pet():
     assert status == 200
     assert pet_ID not in my_pets.values()
 
-    # '''[PUT] / api / pets / {pet_id} — обновление информации о питомце.'''
 
-def test_new_pet_wthout_photo(name='Duran_1', pet_type='Cat', age='4'):
-
-    _, auth_key = pf.get_api_key(login_email, login_pass)
-    status, result = pf.create_simple_pet(auth_key, name, pet_type, age)
-    assert status == 200
-    assert result['name'] == name
